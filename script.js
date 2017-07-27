c9698cf78d69253ad9c7f4cff06dadf7c4025084
Bowen Yin <30426637+BowenYin@users.noreply.github.com>
2017-07-27 13:36:07 -0700

    Update script.js

--- script.js
+++ script.js
@@ -1,33 +1,44 @@
-function hovercard(list) {
-	var hovercard=document.getElementsByClassName("Gp")[0].contentWindow.document;
-	for (var i=0; i<list.length/2; i++) {
-		var index=list.length/2+i;
-		var status='<div><div class="vta">'+list[i].toLowerCase()+"<wbr>@students.harker.org</div></div>"+list[index];
-		if (hovercard.getElementsByClassName("vta")[0].innerHTML.indexOf(list[i].toLowerCase()) != -1) {
-			list[index]=list[index].replace(/class="Xx" tabindex="-1" dir="ltr"/g, 'class="mna"');
-			list[index]=list[index].replace(/<span data-emo="/g, "");
-			while (list[index].indexOf(' vm" style="display:inline-block;"></div') != -1)
-				list[index]=list[index].substring(0, list[index].indexOf('" class="Pt tGvGdc"'))+list[index].substring(list[index].indexOf(' vm" style="display:inline-block;"></div')+48);
-			document.getElementsByClassName("Gp")[0].contentWindow.document.getElementsByClassName("abc Fdc")[0].innerHTML='<div><div class="vta">'+list[i].toLowerCase()+"<wbr>@students.harker.org</div></div>"+list[index];
-			for (var j=0; j<hovercard.getElementsByClassName("mna").length; j++) {
-				var link=hovercard.getElementsByClassName("mna")[j].innerHTML;
+if (window.self=window.top) {
+	window.addEventListener("message", function(event) {
+	if (event.origin !== "https://hangouts.google.com" || event.data.indexOf(",null]") != -1) return;
+	document.getElementsByClassName("Gp")[0].contentWindow.postMessage(event.data, "https://apis.google.com");
+}, false);
+}
+else {
+	var list=[];
+document.addEventListener("mousemove", function() {
+	for (var i=0; i<document.getElementsByClassName("HfhnEf").length; i++)
+	list.push(document.getElementsByClassName("HfhnEf")[i].getAttribute("hovercard-email").slice(0, document.getElementsByClassName("HfhnEf")[i].getAttribute("hovercard-email").indexOf("@")));
+	for (var j=0; j<document.getElementsByClassName("HHWV CRO0lc").length; j++)
+	list.push(document.getElementsByClassName("HHWV CRO0lc")[j].innerHTML);
+	parent.postMessage(list, "https://mail.google.com");
+	list=[];
+}, false);
+	
+	function hovercard(event) {
+	for (var i=0; i<event.data.length/2; i++) {
+		var index=event.data.length/2+i;
+		var status='<div><div class="vta">'+event.data[i].toLowerCase()+"<wbr>@students.harker.org</div></div>"+event.data[index];
+		if (document.getElementsByClassName("vta")[0].innerHTML.indexOf(event.data[i].toLowerCase()) != -1) {
+			event.data[index]=event.data[index].replace(/class="Xx" tabindex="-1" dir="ltr"/g, 'class="mna"');
+			event.data[index]=event.data[index].replace(/<span data-emo="/g, "");
+			while (event.data[index].indexOf(' vm" style="display:inline-block;"></div') != -1)
+				event.data[index]=event.data[index].substring(0, event.data[index].indexOf('" class="Pt tGvGdc"'))+event.data[index].substring(event.data[index].indexOf(' vm" style="display:inline-block;"></div')+48);
+			document.getElementsByClassName("abc Fdc")[0].innerHTML='<div><div class="vta">'+event.data[i].toLowerCase()+"<wbr>@students.harker.org</div></div>"+event.data[index];
+			for (var j=0; j<document.getElementsByClassName("mna").length; j++) {
+				var link=document.getElementsByClassName("mna")[j].innerHTML;
 				for (var k=10; k<link.length; k+=14) {
-					document.getElementsByClassName("Gp")[0].contentWindow.document.getElementsByClassName("mna")[j].innerHTML=link.substring(0, k)+"<wbr>"+link.substring(k);
-					link=hovercard.getElementsByClassName("mna")[j].innerHTML;
+					document.getElementsByClassName("mna")[j].innerHTML=link.substring(0, k)+"<wbr>"+link.substring(k);
+					link=document.getElementsByClassName("mna")[j].innerHTML;
 				}
 			}
 		}
 	}
 }
-var list=[];
-document.addEventListener("mousemove", function() {
-	var frame2=document.getElementsByClassName("a1j")[0].contentWindow.document;
-	for (var i=0; i<frame2.getElementsByClassName("HfhnEf").length; i++)
-	list.push(frame2.getElementsByClassName("HfhnEf")[i].getAttribute("hovercard-email").slice(0, frame2.getElementsByClassName("HfhnEf")[i].getAttribute("hovercard-email").indexOf("@")));
-	for (var j=0; j<frame2.getElementsByClassName("HHWV CRO0lc").length; j++)
-	list.push(frame2.getElementsByClassName("HHWV CRO0lc")[j].innerHTML);
-	setTimeout(function() {hovercard(list)}, 500);
-	setTimeout(function() {hovercard(list)}, 750);
-	setTimeout(function() {hovercard(list)}, 1500);
-	list=[];
+window.addEventListener("message", function(event) {
+	if (event.origin !== "https://mail.google.com" || event.data.indexOf(':".."}') != -1) return;
+	setTimeout(function() {hovercard(event)}, 500);
+	setTimeout(function() {hovercard(event)}, 750);
+	setTimeout(function() {hovercard(event)}, 1500);
 }, false);
+}
